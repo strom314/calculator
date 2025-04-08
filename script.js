e4fa10c60de14c8fe71041806064e73666cc1664
strom314 <jakub.gros777@gmail.com>
2025-04-08 17:57:45 +0200

    create operate function

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+let firstNum
+let secondNum;
+let operator;
+
+function operate(a, b, operator) {
+    let result;
+
+    switch (operator) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+
+        default:
+            result = "error";
+            break;
+    }
+
+    return result
+}
+
+
+
 function add(a, b) {
     return a + b;
 }
