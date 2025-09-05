b24d5f2739bb99fcdc44a4d1a92ab936308be675
strom314 <jakub.gros777@gmail.com>
2025-09-05 16:10:51 +0200

    add functionality for clear and backspace buttons
    create functions and event listeners for these buttons

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ let displayNumber = "";
 const display = document.querySelector(".display");
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator")
+const eqButton = document.querySelector(".equals")
+const clearButtons = document.querySelectorAll(".clear")
+const backspaceButton = document.querySelector(".backspace")
 
 //add event listeners to buttons
 numberButtons.forEach(button => {
@@ -19,6 +22,13 @@ operatorButtons.forEach(button => {
     button.addEventListener("click", operatorButtonPress)
 });
 
+eqButton.addEventListener("click", eqButtonPress)
+
+clearButtons.forEach(button => {
+    button.addEventListener("click", clearButtonPress)
+});
+
+backspaceButton.addEventListener("click", backspaceButtonPress);
 
 function numberButtonPress(button) {
     const pressedButton = button.target.textContent;
@@ -27,19 +37,65 @@ function numberButtonPress(button) {
         firstNum += pressedButton;
     }
     else {
-        secondNum += pressedButtonl;
+        secondNum += pressedButton;
     }
 
-    displayNumber += pressedButton;
+    if (displayNumber == "0") {
+        displayNumber = pressedButton;
+    }
+    else {
+        displayNumber += pressedButton;
+    }
     updateDisplay(displayNumber);
 }
 
 function operatorButtonPress(button) {
     const pressedButton = button.target.textContent;
     operator = pressedButton;
+    isOperatorPressed = true;
 
     displayNumber = "";
-    updateDisplay()
+    updateDisplay(displayNumber)
+}
+
+function eqButtonPress() {
+    let result;
+    if (isNum(firstNum) && isNum(secondNum) && isOperatorPressed) {
+        result = operate(firstNum, secondNum, operator);
+
+        displayNumber = result;
+        updateDisplay(displayNumber);
+
+        //check if there is an error
+        if (isNaN(result)) {
+            return;
+        }
+
+        firstNum = result;
+        secondNum = ""
+        operator = undefined;
+        isOperatorPressed = false;
+    }
+}
+
+function clearButtonPress() {
+    displayNumber = "";
+    firstNum = "";
+    secondNum = "";
+    operator = undefined;
+    updateDisplay(displayNumber);
+}
+
+function backspaceButtonPress() {
+    if (!isOperatorPressed) {
+        firstNum = firstNum.slice(0, -1);
+    }
+    else {
+        secondNum = secondNum.slice(0, -1);
+    }
+
+    displayNumber = displayNumber.slice(0, -1);
+    updateDisplay(displayNumber);
 }
 
 function updateDisplay(displayNumber) {
@@ -48,6 +104,8 @@ function updateDisplay(displayNumber) {
 
 function operate(a, b, operator) {
     let result;
+    a = Number(a)
+    b = Number(b)
 
     switch (operator) {
         case "+":
